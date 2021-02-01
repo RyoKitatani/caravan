400e14121f892ab0b71a563127d813b76f04eb34
RyoKitatani <ryo0929m@gmail.com>
2021-02-01 13:33:39 +0000

    [Finish]4-7.5 デバック

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -14,7 +14,7 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path(blog.id)
+    redirect_to blog_path(blog.id)
   end
 
   def edit
@@ -35,7 +35,7 @@ class BlogsController < ApplicationController
   
   private
   def blog_params
-    params.require(:blog).permit(:title, :categoly, :body)
+    params.require(:blog).permit(:title, :category, :body)
   end
   
 end
